17826ef06c33e2948f7ed9e71d5d807e7a312bb1
amyjoscelyn <amy.joscelyn@flatironschool.com>
2015-11-16 12:21:14 -0500

    added new view

---
+++ RandomFeatures/RandomFeatures/AMYButtonGradientViewController.h
@@ -0,0 +1,13 @@
+//
+//  AMYButtonGradientViewController.h
+//  RandomFeatures
+//
+//  Created by Amy Joscelyn on 11/16/15.
+//  Copyright © 2015 Amy Joscelyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMYButtonGradientViewController : UIViewController
+
+@end
---
+++ RandomFeatures/RandomFeatures/AMYButtonGradientViewController.m
@@ -0,0 +1,82 @@
+//
+//  AMYButtonGradientViewController.m
+//  RandomFeatures
+//
+//  Created by Amy Joscelyn on 11/16/15.
+//  Copyright © 2015 Amy Joscelyn. All rights reserved.
+//
+
+#import "AMYButtonGradientViewController.h"
+
+@interface AMYButtonGradientViewController ()
+
+@end
+
+@implementation AMYButtonGradientViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (IBAction)buttonOneTopGradientTapped:(id)sender
+{
+    
+}
+
+- (IBAction)buttonTwoTopGradientTapped:(id)sender
+{
+    
+}
+
+- (IBAction)buttonThreeTopGradientTapped:(id)sender
+{
+    
+}
+
+- (IBAction)buttonFourTopGradientTapped:(id)sender
+{
+    
+}
+
+- (IBAction)buttonFiveTopGradientTapped:(id)sender
+{
+    
+}
+
+- (IBAction)buttonSixTopGradientTapped:(id)sender
+{
+    
+}
+
+- (IBAction)buttonOneBottomGradientTapped:(id)sender
+{
+    
+}
+
+- (IBAction)buttonTwoBottomGradientTapped:(id)sender
+{
+    
+}
+
+- (IBAction)buttonThreeBottomGradientTapped:(id)sender
+{
+    
+}
+
+- (IBAction)buttonFourBottomGradientTapped:(id)sender
+{
+    
+}
+
+- (IBAction)buttonFiveBottomGradientTapped:(id)sender
+{
+    
+}
+
+- (IBAction)buttonSixBottomGradientTapped:(id)sender
+{
+    
+}
+
+@end
