3315609e2f1055b4a8895bf9a317af70e08fa8bf
amyjoscelyn <amy.joscelyn@flatironschool.com>
2015-11-17 15:46:00 -0500

    all color buttons added as outlets

--- RandomFeatures/RandomFeatures/AMYColorChooserViewController.m
+++ RandomFeatures/RandomFeatures/AMYColorChooserViewController.m
@@ -31,6 +31,36 @@
 @property (weak, nonatomic) IBOutlet UIButton *colorButtonTwoSix;
 @property (weak, nonatomic) IBOutlet UIButton *colorButtonTwoSeven;
 @property (weak, nonatomic) IBOutlet UIButton *colorButtonTwoEight;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonTwoNine;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonTwoTen;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonTwoEleven;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonTwoTwelve;
+
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonThreeOne;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonThreeTwo;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonThreeThree;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonThreeFour;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonThreeFive;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonThreeSix;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonThreeSeven;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonThreeEight;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonThreeNine;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonThreeTen;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonThreeEleven;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonThreeTwelve;
+
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonFourOne;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonFourTwo;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonFourThree;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonFourFour;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonFourFive;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonFourSix;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonFourSeven;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonFourEight;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonFourNine;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonFourTen;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonFourEleven;
+@property (weak, nonatomic) IBOutlet UIButton *colorButtonFourTwelve;
 
 @end
 
